0abc1e1abd77f1b5a31c5fe7ead42c3524e9f276
Steve <stevepesce879@gmail.com>
2020-12-15 12:43:18 -0500

    made calculator

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -6,10 +6,39 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            int x = 20;
-            int y = 35;
-            int a = x + y;
-            Console.WriteLine(x + " + " + y + " = " + a);
+            int x = 0; int y = 0;
+
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("-------------------------\n");
+
+            Console.WriteLine("Type a number, then press Enter");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type another number, then press Enter");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\t+ - Add");
+            Console.WriteLine("\t- - Subtract");
+            Console.WriteLine("\t* - Multiply");
+            Console.WriteLine("\t/ - Divide");
+            Console.Write("Your option? ");
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    Console.WriteLine($"Result: {x} + {y} = " + (x + y));break;
+                case "-":
+                    Console.WriteLine($"Result: {x} - {y} = " + (x - y));break;
+                case "*":
+                    Console.WriteLine($"Result: {x} * {y} = " + (x * y));break;
+                case "/":
+                    Console.WriteLine($"Result: {x} / {y} = " + (x / y));break;
+                default:
+                    Console.WriteLine("Bad Input");break;
+            }
+
+
         }
     }
 }
