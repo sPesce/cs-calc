641565902c36e89d63000fe57267e6b370d39426
Steve <stevepesce879@gmail.com>
2020-12-15 12:33:44 -0500

    testing ints

---
+++ calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            int y = 35;
+            int a = x + y;
+            Console.WriteLine(x + " + " + y + " = " + a);
+        }
+    }
+}
